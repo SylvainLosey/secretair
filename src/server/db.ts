ba5e23515e97a4c7861b57c3f7df511c3009dfff
Sylvain Losey <sylvainlosey@gmail.com>
2025-03-01 23:07:01 +0100

    Fix fix fix

--- src/server/db.ts
+++ src/server/db.ts
@@ -4,16 +4,19 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db = globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-    // For PgBouncer compatibility
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
+// Create a client with pgBouncer option
+const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL + "?pgbouncer=true",
     },
-  });
+  },
+});
 
+// Export the client
+export const db = globalForPrisma.prisma ?? prisma;
+
+// Save to global in development for hot reloading
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
